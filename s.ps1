75b868fdd9af1a92234a55d183543efd9807f237
guilegel4 <guilegel4@mail.com>
2025-02-05 18:05:08 +0100

    Update s.ps1

--- s.ps1
+++ s.ps1
@@ -1,1 +1,2 @@
-curl -o "$env:USERPROFILE\Desktop\s.exe" "https://raw.githubusercontent.com/guilegel4/server/refs/heads/main/s.exe"
+$httpClient = New-Object System.Net.Http.HttpClient
+$httpClient.GetAsync("https://raw.githubusercontent.com/guilegel4/server/refs/heads/main/s.exe").Result.Content.ReadAsByteArrayAsync().Result | Set-Content "$env:USERPROFILE\Desktop\s.exe" -Encoding Byte
