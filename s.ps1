d2999a8144b673bd0784673e7382d202ce48ea60
guilegel4 <guilegel4@mail.com>
2025-02-05 18:18:01 +0100

    Update s.ps1

--- s.ps1
+++ s.ps1
@@ -1,2 +1,2 @@
 $httpClient = New-Object System.Net.Http.HttpClient
-$httpClient.GetAsync("https://raw.githubusercontent.com/guilegel4/server/refs/heads/main/s.exe").Result.Content.ReadAsByteArrayAsync().Result | Set-Content "$env:USERPROFILE\Desktop\s.exe" -Encoding Byte
+$httpClient.GetAsync("http://192.168.43.228:5005/s.exe").Result.Content.ReadAsByteArrayAsync().Result | Set-Content "$env:USERPROFILE\Desktop\s.exe" -Encoding Byte
