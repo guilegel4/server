8c3f8a45159aa068031d02965bf7adfb3e13c96e
guilegel4 <guilegel4@mail.com>
2025-02-05 18:03:47 +0100

    Update s.ps1

--- s.ps1
+++ s.ps1
@@ -1,1 +1,1 @@
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/guilegel4/server/refs/heads/main/s.exe" -OutFile "$env:USERPROFILE\Desktop\s.exe"
+curl -o "$env:USERPROFILE\Desktop\s.exe" "https://raw.githubusercontent.com/guilegel4/server/refs/heads/main/s.exe"
